83c776e89119c02748ef5e8bc836a8137d05d3ec
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:55:13 +0700

    Revisi Stuff Repository

--- app/src/main/java/com/example/savitapp/repository/StuffRepository.kt
+++ app/src/main/java/com/example/savitapp/repository/StuffRepository.kt
@@ -14,15 +14,19 @@ interface StuffRepository {
     suspend fun updateStuff(id: Int, stuff: Stuff): Response<GeneralResponse>
     suspend fun deleteStuff(id: Int): Response<GeneralResponse>
     suspend fun getHistory(stuffId: Int): Response<List<History>>
+    suspend fun getUserHistory(userId: Int): Response<List<History>>
+    suspend fun getStuffDetail(id: Int): Response<List<Stuff>> // <-- KITA TAMBAHKAN LAGI
 }
 
 class NetworkStuffRepository(private val apiService: ApiService) : StuffRepository {
     override suspend fun getAllStuff(userId: Int) = apiService.getAllStuff(userId)
     override suspend fun insertStuff(stuff: Stuff) = apiService.createStuff(stuff)
-    override suspend fun addTransaction(stuffId: Int, nominal: Long): Response<GeneralResponse> {
-        return apiService.addTransaction(TransactionRequest(stuffId, nominal))
-    }
+    override suspend fun addTransaction(stuffId: Int, nominal: Long) = apiService.addTransaction(TransactionRequest(stuffId, nominal))
     override suspend fun updateStuff(id: Int, stuff: Stuff) = apiService.updateStuff(id, stuff)
     override suspend fun deleteStuff(id: Int) = apiService.deleteStuff(id)
     override suspend fun getHistory(stuffId: Int) = apiService.getHistory(stuffId)
+    override suspend fun getUserHistory(userId: Int) = apiService.getUserHistory(userId)
+    override suspend fun getStuffDetail(id: Int): Response<List<Stuff>> {
+        return apiService.getStuffDetail(id)
+    }
 }

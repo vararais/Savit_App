b2493ef034eea235c59b1fe5b4f6d3a495f42099
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:52:43 +0700

    Perbaikan error Entry View Screen

--- app/src/main/java/com/example/savitapp/view/home/EntryViewScreen.kt
+++ app/src/main/java/com/example/savitapp/view/home/EntryViewScreen.kt
@@ -19,8 +19,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.savitapp.model.InsertUiEvent
 import com.example.savitapp.viewmodel.EntryViewModel
-import com.example.savitapp.viewmodel.InsertUiEvent
 import com.example.savitapp.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 

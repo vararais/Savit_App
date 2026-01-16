674cffb75cb58ba346547fe85813aeb735ed4db2
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:51:39 +0700

    Perbaikan error Api Service

--- app/src/main/java/com/example/savitapp/service/ApiService.kt
+++ app/src/main/java/com/example/savitapp/service/ApiService.kt
@@ -1,40 +1,39 @@
 package com.example.savitapp.service
 
-import com.example.savitapp.model.TransactionRequest
-import com.example.savitapp.model.GeneralResponse
-import com.example.savitapp.model.Stuff
 import com.example.savitapp.model.*
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.POST
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.DELETE
-import retrofit2.http.PUT
-
+import retrofit2.http.*
 
 interface ApiService {
-    @POST("auth/login")
-    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
-
     @POST("auth/register")
     suspend fun register(@Body request: RegisterRequest): Response<RegisterResponse>
 
-    @GET("stuff/{user_id}")
-    suspend fun getAllStuff(@Path("user_id") userId: Int): Response<List<Stuff>>
+    @POST("auth/login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+
+    // KEMBALIKAN KE INI (Hapus 'user/'):
+    @GET("stuff/{userId}")
+    suspend fun getAllStuff(@Path("userId") userId: Int): Response<List<Stuff>>
+
+    // Biarkan ini (Walaupun tidak akan kita pakai lagi di ViewModel):
+    @GET("stuff/{id}")
+    suspend fun getStuffDetail(@Path("id") id: Int): Response<List<Stuff>>
 
     @POST("stuff")
-    suspend fun createStuff(@Body stuff: Stuff): Response<GeneralResponse>
+    suspend fun createStuff(@Body request: Stuff): Response<GeneralResponse>
 
     @POST("transaction")
     suspend fun addTransaction(@Body request: TransactionRequest): Response<GeneralResponse>
 
     @PUT("stuff/{id}")
-    suspend fun updateStuff(@Path("id") id: Int, @Body stuff: Stuff): Response<GeneralResponse>
+    suspend fun updateStuff(@Path("id") id: Int, @Body request: Stuff): Response<GeneralResponse>
 
     @DELETE("stuff/{id}")
     suspend fun deleteStuff(@Path("id") id: Int): Response<GeneralResponse>
 
-    @GET("transaction/history/{stuff_id}")
-    suspend fun getHistory(@Path("stuff_id") stuffId: Int): Response<List<History>>
+    @GET("transaction/{stuffId}")
+    suspend fun getHistory(@Path("stuffId") stuffId: Int): Response<List<History>>
+
+    @GET("transaction/history/{userId}")
+    suspend fun getUserHistory(@Path("userId") userId: Int): Response<List<History>>
 }
